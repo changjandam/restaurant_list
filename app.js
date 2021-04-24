74ba9a8aea22f297ab252491d66e15ff70d8c015
changjandam <changjandamtw@gmail.com>
2021-04-25 01:17:43 +0800

    project init

---
+++ app.js
@@ -0,0 +1,39 @@
+//set express and express-handlebars
+const express = require('express')
+const app = express()
+const expressHandlebars = require('express-handlebars')
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//set public file
+app.use(express.static('public'))
+
+//import restaurant list
+const restaurantList = require('./restaurant.json').results
+
+//set path
+app.get('/', (req, res) => {
+  res.render('index', { restaurantList })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const showRestaurant = restaurantList[parseInt(req.params.restaurant_id) - 1]
+  res.render('show', { showRestaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const matchedRestaurant = restaurantList.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) | restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+  res.render('index', { restaurantList: matchedRestaurant, keyword })
+})
+
+
+
+
+//set port
+const port = 3000
+app.listen(port, () => {
+  console.log(`The web app is running on http://localhost:${port}`)
+})
+
+
